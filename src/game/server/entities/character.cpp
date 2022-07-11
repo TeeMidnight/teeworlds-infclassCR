1b1d5b62f81294f52a9ea2ef554f8d7a0a84e773
ErrorDreemurr <diandaokui@qq.com>
2022-07-11 11:10:22 +0800

    修复bug测试

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -1417,7 +1417,7 @@ void CCharacter::FireWeapon()
 			}
 			else if(GetClass() == PLAYERCLASS_SCIOGIST)
 			{
-				float Spreading[] = {-0.42f, -0.21f, 0.0f};
+				float Spreading[] = {-0.63f, -0.21f, 0.21f};
 				float angle = GetAngle(Direction);
 				for(int i = 1;i <= 3;i++)
 				{
@@ -1715,7 +1715,7 @@ void CCharacter::HandleWeapons()
 		
 	//ninja
 	HandleNinja();
-
+	
 	//slime
 	if(m_ReloadSlimeTick)
 	{
